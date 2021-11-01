3449f35b5bffcb40ce0a63da4b292e8026535d3b
André <alaubscher22@gmail.com>
2021-11-02 01:25:03 +0200

    Added piping for components

---
+++ ClientApp/src/app/index.ts
@@ -0,0 +1,11 @@
+import { UserComponent } from "./user/user"
+import { UserTableComponent } from './user-table/user-table.component';
+
+export { UserComponent };
+export { UserTableComponent };
+
+export const COMPONENTS =
+{
+  UserComponent,
+  UserTableComponent
+}
